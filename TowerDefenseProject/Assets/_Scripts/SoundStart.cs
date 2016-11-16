82bf6de011551335936e6ddadda9cd478e35702d
Moritz <horn.moritz@web.de>
2016-11-16 11:55:43 +0100

    Added Mastervolume for StartMenu

---
+++ TowerDefenseProject/Assets/_Scripts/SoundStart.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundStart : MonoBehaviour {
+
+    private MusicManager musicManager;
+
+    // Use this for initialization
+    void Start () {
+        musicManager = GameObject.FindObjectOfType<MusicManager>();
+        musicManager.ChangeVolume(PlayerPrefsManager.GetMasterVolume());
+	}
+}
