faf0c5138143308bb8356a612b31e46a4d04ba68
Moritz <horn.moritz@web.de>
2016-11-23 16:45:33 +0100

    Added Tarrain for Menu

---
+++ TowerDefenseProject/Assets/_Scenes/_Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start(){
+		if(autoLoadNextLevelAfter != 0){
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+
+	public void LoadLevel(string name) {		
+		Debug.Log ("Level load requested for: " + name);
+        SceneManager.LoadScene(name);
+	}
+
+	public void QuitRequest() {
+		Debug.Log ("I Want to quit!");
+		Application.Quit ();
+	}
+}
---
+++ TowerDefenseProject/Assets/_Scenes/_Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPrefsManager : MonoBehaviour {
+
+	const string MASTER_VOLUME_KEY = "master_volume";
+	const string DIFF_KEY = "difficulty";
+
+	public static void SetMasterVolume(float volume) {
+		if(volume >= 0f && volume <= 1f){
+			PlayerPrefs.SetFloat (MASTER_VOLUME_KEY, volume);
+		}
+		else {
+			Debug.LogError ("Master volume out of range");
+		}
+	}
+
+	public static float GetMasterVolume(){
+		return PlayerPrefs.GetFloat (MASTER_VOLUME_KEY);
+	}
+
+	public static void SetDifficulty (float diff){
+		if(diff >= 0f && diff <= 3f){
+			PlayerPrefs.SetFloat (DIFF_KEY, diff);
+		} 
+		else {
+			Debug.Log ("Difficulty out of range");
+		}
+	}
+
+	public static float GetDifficulty(){
+		return PlayerPrefs.GetFloat (DIFF_KEY);
+	}
+}
