bdeccf86fbfed484e84df2e09de56b3121ed685e
Moritz <horn.moritz@web.de>
2016-11-26 11:33:55 +0100

    no message

---
+++ TowerDefenseProject/Assets/_Scripts/MusicManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+
+	private AudioSource audioSource;
+
+	void Awake(){
+		DontDestroyOnLoad (gameObject);
+	}
+
+	void Start(){
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	void OnLevelWasLoaded(int level){
+
+		AudioClip thisLevelMusic = levelMusicChangeArray [level];
+		Debug.Log ("Playing Clip: " + thisLevelMusic);
+
+		if(thisLevelMusic){
+			audioSource.clip = thisLevelMusic;
+			audioSource.loop = true;
+            audioSource.volume = 0.1f;
+			audioSource.Play ();
+		}
+	}
+
+	public void ChangeVolume(float vol){
+		audioSource.volume = vol;
+	}
+}
---
+++ TowerDefenseProject/Assets/_Scripts/OptionsController.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OptionsController : MonoBehaviour {
+
+	public Slider volumeSlider;
+	public Slider diffSlider;
+	public LevelManager levelManager;
+    public float speed;
+
+    private MusicManager musicManager;
+    private bool optionPressed;
+    private GameObject optionCanvas;
+    private GameObject startCanvas;
+    private GameObject world;
+    private bool worldLeft;
+
+	// Use this for initialization
+	void Start () {
+		musicManager = GameObject.FindObjectOfType<MusicManager> ();
+        musicManager.ChangeVolume(PlayerPrefsManager.GetMasterVolume());
+        optionCanvas = GameObject.Find("CanvasOptions");
+        startCanvas = GameObject.Find("CanvasStart");
+        world = GameObject.Find("World");
+        worldLeft = true;
+        optionCanvas.SetActive(false);
+    }
+	
+ 	public void BackAndSave(){
+		PlayerPrefsManager.SetMasterVolume (volumeSlider.value);
+		PlayerPrefsManager.SetDifficulty (diffSlider.value);
+        optionPressed = false;
+        optionCanvas.SetActive(false);
+        startCanvas.SetActive(true);
+		// Close Options
+	}
+
+	void Update() {
+        // When optionmenu is open, change Mastervolume to valid Slider
+        if (optionPressed)
+        {
+            musicManager.ChangeVolume (volumeSlider.value);
+        }
+        moveBackgroundWorld();
+	}
+
+	public void SetDefault(){
+		volumeSlider.value = 0.2f;
+		diffSlider.value = 1f;
+	}
+
+    public void OptionPress()
+    {
+        // Öffne Canvas
+        Debug.Log("Open Options");
+        Debug.Log(optionCanvas);
+        optionPressed = true;
+        optionCanvas.SetActive(true);
+        startCanvas.SetActive(false);
+        volumeSlider.value = PlayerPrefsManager.GetMasterVolume();
+        diffSlider.value = PlayerPrefsManager.GetDifficulty();
+    }
+
+    private void moveBackgroundWorld()
+    {
+        if (optionPressed)
+        {
+            // go right
+            if (world.transform.position.x >= 90f)
+            {
+                return;
+            }
+            world.transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+        } 
+        else
+        {
+            // go left
+            if (world.transform.position.x <= -70f)
+            {
+                return;
+            }
+            world.transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        }
+    }
+}
