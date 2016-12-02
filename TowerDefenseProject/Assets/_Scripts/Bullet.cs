ae1c07e396abc4395a6f4848f71235bc153835a9
Moritz <horn.moritz@web.de>
2016-12-02 15:04:58 +0100

    Added Turret and Bullet Script + ParticleSystem

---
+++ TowerDefenseProject/Assets/_Scripts/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	private Transform target;
+	public float speed = 70f;
+	public GameObject impactEffect;
+
+	public void SetTarget(Transform _target) {
+		target = _target;
+	}
+
+	void Update () {
+		if(target == null) {
+			Destroy (gameObject);
+			return;
+		}
+
+		Vector3 dir = target.position - transform.position;
+		float distanceThisFrame = speed * Time.deltaTime;
+
+		// Bullet hit target
+		if(dir.magnitude <= distanceThisFrame) {
+			HitTarget ();
+		}
+
+		transform.Translate (dir.normalized * distanceThisFrame, Space.World);
+	}
+
+	void HitTarget() {
+		GameObject effIns = Instantiate (impactEffect, transform.position, transform.rotation) as GameObject;
+		Destroy (effIns, 2f);
+
+		Destroy (target.gameObject);
+
+		Destroy (gameObject);
+	}
+}
--- TowerDefenseProject/Assets/_Scripts/Turret.cs
+++ TowerDefenseProject/Assets/_Scripts/Turret.cs
@@ -4,9 +4,18 @@ using System.Collections;
 public class Turret : MonoBehaviour {
 
 	private Transform target;
+
+	[Header("Attributes")]
 	public float range = 15f;
-	public Transform partToRotate;
 	public float turnSpeed = 10f;
+	// shots per second
+	public float fireRate = 1f;
+
+	[Header("Unity Setup Fields")]
+	private float fireCountdown = 0f;
+	public Transform partToRotate;
+	public GameObject bulletPrefab;
+	public Transform firePoint;
 
 	void Start() {
 		InvokeRepeating ("UpdateTarget", 0f, 0.5f);
@@ -42,6 +51,20 @@ public class Turret : MonoBehaviour {
 		Quaternion lookRotation = Quaternion.LookRotation (dir);
 		Vector3 rotation = Quaternion.Lerp (partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
 		partToRotate.rotation = Quaternion.Euler (0f, rotation.y, 0f);
+
+		if(fireCountdown <= 0f) {
+			Shoot ();
+			fireCountdown = 1f / fireRate;
+		}
+		fireCountdown -= Time.deltaTime;
+	}
+
+	void Shoot() {
+		GameObject bulletGO = Instantiate (bulletPrefab, firePoint.position, firePoint.rotation) as GameObject;
+		Bullet bullet = bulletGO.GetComponent<Bullet> ();
+		if(bullet != null) {
+			bullet.SetTarget (target);
+		}
 	}
 
 	void OnDrawGizmosSelected() {
