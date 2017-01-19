aa3106edbed3743442a568602c5705d9582a6acb
Moritz Horn <horn.moritz@web.de>
2017-01-19 17:54:15 +0100

    no message

---
+++ TowerDefenseProject/Assets/LeapTestScripts/Gestures.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gestures : MonoBehaviour {
+
+
+
+	public void OpenTowerMenu(){
+		Debug.Log ("Open Tower Menu");
+	}
+
+	public void CloseTowerMenu(){
+		Debug.Log ("Close Menu");
+	}
+}
